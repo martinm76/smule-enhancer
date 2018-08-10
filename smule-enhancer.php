29bf6232fc142b094abae09f6650c0411d705527
martinm76 <martin.moller@gmail.com>
2018-08-10 22:55:06 +0200

    Initial working version for Wordpress
    This is the first version I managed to get to work in WordPress. After you install the plugin and activate it, it should automatically embed (via iframe) the song you chose, given it follows this format: https://www.smule.com/recording/artist-and-title-of-song/numeric_id_of_recording, e.g. : https://www.smule.com/recording/a-great-big-world-say-something-stripped-acoustic-cover/1030153185_2335589594
    
    Further enhancements may come down the line.

---
+++ smule-enhancer.php
@@ -0,0 +1,51 @@
+<?php 
+/* 
+ * Plugin Name: Smule enhancement features and functions 
+ * Plugin URI: N/A
+ * Description: Automatically embed smule recordning URLs, if they are in the right format.
+ * Version: 1.00
+ * Author: Martin Møller <martin.moller@gmail.com> 
+ * Author URI: https://github.com/martinm76
+ * License: GPL2
+ * -=-
+ *
+ * Tried oemhed of recordings for automatic embedding. Taken from 
+ * https://www.smule.com/support/embedding - couldn't get it to work.
+ * Attempted a regular embed in stead.
+ * --
+ * martin.moller@gmail.com
+ */
+
+// Register oEmbed providers
+/*
+function smule_oembed_provider() {
+
+	wp_oembed_add_provider( 'https://www.smule.com/recording/*', 'https://www.smule.com/s/oembed', false );
+
+}
+add_action( 'init', 'smule_oembed_provider' );
+*/
+
+add_action( 'init', function()
+{
+    wp_embed_register_handler( 
+        'smule', 
+        '#https://www\.smule\.com/recording/([a-zA-Z0-9_-]+)/([a-zA-Z0-9_-]+)$#i',
+        'smule_embed_handler' 
+    );
+} );
+
+/**
+ * Handle callback for the smule karaoke provider
+ */
+function smule_embed_handler( $matches, $attr, $url, $rawattr )
+{
+	$embed = sprintf(
+			'<iframe src="https://www.smule.com/recording/%1$s/%2$s/frame" width="450" height="125" frameborder="0" scrolling="no" marginwidth="0" marginheight="0"></iframe>',
+			esc_attr($matches[1]),
+			esc_attr($matches[2])
+			);
+    return apply_filters( 'smule_embed_handler', $embed, $matches, $attr, $url, $rawattr );
+}
+
+?>
